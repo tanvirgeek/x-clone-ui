b37c58f82550258c21c24c8d71cf7c01bf237e0a
Tanvirgeek <tanvirgeek@gmail.com>
2025-02-26 06:20:35 +0600

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
           <div className="px-2 xsm:px-4 xxl:px-8">
             <LeftBar />
           </div>
-          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100">
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
             {children}
           </div>
           <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 const LeftBar = () => {
   return (
-    <div>LeftBar</div>
-  )
-}
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* LOGO */}
+      <div className="flex flex-col gap-4 items-center xxl:items-start">
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
+        </Link>
 
-export default LeftBar
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              href={item.link}
+              className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+              key={item.id}
+            >
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                width={24}
+                height={24}
+              />
+              <span className="hidden xxl:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      {/* USER */}
+      <div className="">User</div>
+    </div>
+  );
+};
+
+export default LeftBar;
